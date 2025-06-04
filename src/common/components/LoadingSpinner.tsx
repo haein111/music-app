0707c6a796c7f915ec245df7019146eda9f7c266
haein111 <junghi375@gmail.com>
2025-06-04 12:27:24 +0900

    add Navbar

--- src/common/components/LoadingSpinner.tsx
+++ src/common/components/LoadingSpinner.tsx
@@ -1,8 +1,37 @@
 import React from "react";
-import "./LoadingSpinner.css";
+// import "./LoadingSpinner.css";
+import { keyframes, styled } from "@mui/material";
+
+const rotation = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(359deg);
+  }
+`;
+
+const Spinner = styled("div")({
+  width: "200px",
+  height: "200px",
+  borderRight: "6px solid #fff",
+  borderRadius: "50%",
+  animation: `${rotation} 2s infinite linear`,
+});
 
 function LoadingSpinner() {
-  return <div className="loading-spinner" />;
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <Spinner />
+    </div>
+  );
 }
 
 export default LoadingSpinner;
---
+++ src/common/components/LoginButton.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@mui/material";
+import React from "react";
+
+function LoginButton() {
+  return (
+    <Button variant="contained" color="secondary" size="large">
+      Login
+    </Button>
+  );
+}
+
+export default LoginButton;
