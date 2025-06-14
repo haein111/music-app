a0965fb7dc4834d936aad170e592c8175b0d3f2b
haein111 <junghi375@gmail.com>
2025-06-14 20:21:43 +0900

    add style

--- src/pages/PlaylistDetailPage/components/SearchResultList.tsx
+++ src/pages/PlaylistDetailPage/components/SearchResultList.tsx
@@ -16,18 +16,17 @@ import { useEffect } from "react";
 
 const AlbumContainer = styled(TableRow)(({ theme }) => ({
   width: "100%",
-  margin: "0 10px",
 }));
-
-interface SearchResultListProps {
-  list: Track[];
-}
 
 const AlbumImage = styled("img")(({ theme }) => ({
   borderRadius: "6px",
   width: "50px",
   marginRight: "8px",
 }));
+
+interface SearchResultListProps {
+  list: Track[];
+}
 
 const SearchResultList = ({
   list,
@@ -46,40 +45,42 @@ const SearchResultList = ({
   return (
     <div>
       {list.map((track) => (
-        <AlbumContainer key={track.id}>
-          <TableCell>
-            <Box display={"flex"} alignItems="center">
-              <Box>
-                <AlbumImage
-                  src={
-                    track.album &&
-                    track.album.images &&
-                    track.album.images.length > 0
-                      ? track.album.images[0].url
-                      : "default-image-url.jpg"
-                  }
-                  alt="Album cover"
-                />
+        <Table sx={{ tableLayout: "fixed", width: "100%" }}>
+          <AlbumContainer key={track.id}>
+            <TableCell>
+              <Box display={"flex"} alignItems="center">
+                <Box>
+                  <AlbumImage
+                    src={
+                      track.album &&
+                      track.album.images &&
+                      track.album.images.length > 0
+                        ? track.album.images[0].url
+                        : "default-image-url.jpg"
+                    }
+                    alt="Album cover"
+                  />
+                </Box>
+                <Box>
+                  <Typography variant="h2" fontWeight={700}>
+                    {track.name}
+                  </Typography>
+                  <Typography variant="body1" color="textSecondary">
+                    {track.artists && track.artists.length > 0
+                      ? track.artists[0]?.name
+                      : "Unknown"}
+                  </Typography>
+                </Box>
               </Box>
-              <Box>
-                <Typography variant="h2" fontWeight={700}>
-                  {track.name}
-                </Typography>
-                <Typography variant="body1" color="textSecondary">
-                  {track.artists && track.artists.length > 0
-                    ? track.artists[0]?.name
-                    : "Unknown"}
-                </Typography>
-              </Box>
-            </Box>
-          </TableCell>
-          <TableCell>
-            <Typography variant="h2">{track.album?.name}</Typography>
-          </TableCell>
-          <TableCell>
-            <Button>Add</Button>
-          </TableCell>
-        </AlbumContainer>
+            </TableCell>
+            <TableCell>
+              <Typography variant="h2">{track.album?.name}</Typography>
+            </TableCell>
+            <TableCell>
+              <Button>Add</Button>
+            </TableCell>
+          </AlbumContainer>
+        </Table>
       ))}
 
       {/* 무한 스크롤 */}
